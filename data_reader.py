19f615915cfb1f1e7c7eca5949ea5406e98b74a9
Arif <80994072+arf-themascoteers@users.noreply.github.com>
2022-09-21 10:09:30 +1000

    done

--- data_reader.py
+++ data_reader.py
@@ -8,17 +8,15 @@ data = pd.read_csv("data/amazon.csv")
 price = data[['Close']]
 scaler = MinMaxScaler(feature_range=(-1, 1))
 price['Close'] = scaler.fit_transform(price['Close'].values.reshape(-1,1))
+LOOKBACK = 20
 
 
-def split_data(stock, lookback):
-    data_raw = stock.to_numpy()  # convert to numpy array
+def split_data(stock):
+    data_raw = stock.to_numpy()
     data = []
 
-    #data_raw = data_raw[:,0]
-
-    # create all possible sequences of length seq_len
-    for index in range(len(data_raw) - lookback):
-        data.append(data_raw[index: index + lookback])
+    for index in range(len(data_raw) - LOOKBACK):
+        data.append(data_raw[index: index + LOOKBACK])
 
     data = np.array(data);
     test_set_size = int(np.round(0.2 * data.shape[0]));
@@ -30,18 +28,17 @@ def split_data(stock, lookback):
     x_test = data[train_set_size:, :-1]
     y_test = data[train_set_size:, -1]
 
-
     return [x_train, y_train, x_test, y_test]
 
 
 def get_data():
-    lookback = 20  # choose sequence length
-    x_train, y_train, x_test, y_test = split_data(price, lookback)
+    x_train, y_train, x_test, y_test = split_data(price)
     x_train = torch.from_numpy(x_train).type(torch.Tensor)
     x_test = torch.from_numpy(x_test).type(torch.Tensor)
     y_train = torch.from_numpy(y_train).type(torch.Tensor)
     y_test = torch.from_numpy(y_test).type(torch.Tensor)
     return x_train, y_train, x_test, y_test
+
 
 if __name__ == "__main__":
     get_data()
